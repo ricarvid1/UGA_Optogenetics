fdd76df125f982a10f8dd5ab7e8da1c1b705953a
DAVID_UGA <david-ricardo.sanchez-jacome@etu.univ-amu.fr>
2018-06-13 17:45:57 +0200

    GUI with all its parts implemented. Functionality for the for the Camera and ROI sections were also implemented.

--- TIRF_GUI.py
+++ TIRF_GUI.py
@@ -6,106 +6,184 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 import numpy as np
+from AcquisitionModel import AcquisitionModel
 
 
-class window(QWidget):
+class Window(QWidget):
 
     def __init__(self):
-        super(window, self).__init__()
-        self.setGeometry(100, 200, 750, 550)
-        self.setWindowTitle('TIRF')
-        # self.setWindowIcon(QIcon('pic.png'))
+        super(Window, self).__init__()
+        self.setGeometry(100, 200, 900, 650)
+        self.setWindowTitle('TIRF Experiment Manager')
+        self.setWindowIcon(QIcon('logo_LIPhy.png'))
+        self.cameraModel = AcquisitionModel()
+        self.snapImage()
         self.home()
 
     def home(self):
-
+        #Left Section
         # Left boxes are added here
         vbox_main = QVBoxLayout()
 
+        # Adding elements to the left main box
+        vbox_main.addStretch()
+        vbox_main.addWidget(self.setCamHome())
+        vbox_main.addStretch()
+        vbox_main.addWidget(self.setROIHome())
+        vbox_main.addStretch()
+        vbox_main.addWidget(self.setDMDHome())
+        vbox_main.addStretch()
+
+        # Right main Section
+        #Main right VBox
+        vbox_main_exp = QVBoxLayout()
+        vbox_main_exp.addWidget(self.setGraphHome())
+        vbox_main_exp.addStretch()
+        vbox_main_exp.addWidget(self.setExpHome())
+
+        #Main HBOX containing all subelements
+        hbox_main = QHBoxLayout()
+        hbox_main.addLayout(vbox_main)
+        hbox_main.addStretch()
+        hbox_main.addLayout(vbox_main_exp)
+        self.setLayout(hbox_main)
+        # Showing all the graphs
+        self.show()
+
+    def setCamHome(self):
         # Camera section is added using the grid option
         # Labels
-        #lbl_cam = QLabel("Camera")
+        # lbl_cam = QLabel("Camera")
         lbl_cam_exp = QLabel("Exposure Time")
         lbl_cam_ms = QLabel("ms")
         lbl_cam_num = QLabel("Number of Im. ")
         # Edit lines
-        edt_cam_exp = QLineEdit()
-        edt_cam_exp.setValidator(QIntValidator())
-        edt_cam_exp.setAlignment(Qt.AlignRight)
-        edt_cam_exp.setMaxLength(4)
-        edt_cam_exp.resize(10, 50)
-        edt_cam_num = QLineEdit()
-        edt_cam_num.setValidator(QIntValidator())
-        edt_cam_num.setAlignment(Qt.AlignRight)
-        edt_cam_num.setMaxLength(4)
+        self.edt_cam_exp = QLineEdit()
+        self.edt_cam_exp.setValidator(QIntValidator())
+        self.edt_cam_exp.setAlignment(Qt.AlignRight)
+        self.edt_cam_exp.setMaxLength(4)
+        self.edt_cam_exp.setText('1')
+        self.edt_cam_num = QLineEdit()
+        self.edt_cam_num.setValidator(QIntValidator())
+        self.edt_cam_num.setAlignment(Qt.AlignRight)
+        self.edt_cam_num.setMaxLength(4)
+        self.edt_cam_num.setText('1')
         # Buttons
-        btn_cam_roi = QPushButton("Select ROI")
-        btn_cam_acq = QPushButton("Acquire")
+        self.btn_cam_roi = QPushButton("Select ROI")
+        self.btn_cam_acq = QPushButton("Acquire")
+        self.btn_cam_acq.clicked.connect(self.startSequenceAcquisition)
         # grid
         grid_cam = QGridLayout()
-        #grid_cam.addWidget(lbl_cam, 0, 0, 1, 1)
+        # grid_cam.addWidget(lbl_cam, 0, 0, 1, 1)
         grid_cam.addWidget(lbl_cam_exp, 1, 0, 1, 1)
-        grid_cam.addWidget(edt_cam_exp, 1, 1, 1, 1)
+        grid_cam.addWidget(self.edt_cam_exp, 1, 1, 1, 1)
         grid_cam.addWidget(lbl_cam_ms, 1, 2, 1, 1)
-        grid_cam.addWidget(btn_cam_roi, 2, 0, 1, 2)
-        grid_cam.addWidget(lbl_cam_num, 3, 0, 1, 1)
-        grid_cam.addWidget(edt_cam_num, 3, 1, 1, 1)
-        grid_cam.addWidget(btn_cam_acq, 4, 0, 1, 2)
+        #grid_cam.addWidget(self.btn_cam_roi, 2, 0, 1, 2)
+        grid_cam.addWidget(lbl_cam_num, 2, 0, 1, 1)
+        grid_cam.addWidget(self.edt_cam_num, 2, 1, 1, 1)
+        grid_cam.addWidget(self.btn_cam_acq, 3, 0, 1, 2)
         # GroupBox
         group_cam = QGroupBox("Camera")
         group_cam.setLayout(grid_cam)
+        return group_cam
 
+    def setROIHome(self):
+        # Camera section is added using the grid option
+        # Labels
+        # lbl_cam = QLabel("Camera")
+        lbl_roi_x = QLabel("X Coordinate")
+        lbl_roi_y = QLabel("X Coordinate")
+        lbl_roi_width = QLabel("ROI Width")
+        lbl_roi_height = QLabel("ROI Height")
+        # Edit lines
+        self.edt_roi_x = QLineEdit()
+        self.edt_roi_x.setValidator(QIntValidator(0, self.cameraModel.getXSize()))
+        self.edt_roi_x.setAlignment(Qt.AlignRight)
+        self.edt_roi_x.setMaxLength(4)
+        self.edt_roi_x.setText('0')
+        self.edt_roi_y = QLineEdit()
+        self.edt_roi_y.setValidator(QIntValidator(0, self.cameraModel.getYSize()))
+        self.edt_roi_y.setAlignment(Qt.AlignRight)
+        self.edt_roi_y.setMaxLength(4)
+        self.edt_roi_y.setText('0')
+        self.edt_roi_width = QLineEdit()
+        self.edt_roi_width.setValidator(QIntValidator(0, self.cameraModel.getXSize()))
+        self.edt_roi_width.setAlignment(Qt.AlignRight)
+        self.edt_roi_width.setMaxLength(4)
+        self.edt_roi_width.setText(str(self.cameraModel.getXSize()))
+        self.edt_roi_height = QLineEdit()
+        self.edt_roi_height.setValidator(QIntValidator(0, self.cameraModel.getYSize()))
+        self.edt_roi_height.setAlignment(Qt.AlignRight)
+        self.edt_roi_height.setMaxLength(4)
+        self.edt_roi_height.setText(str(self.cameraModel.getYSize()))
+        # Buttons
+        self.btn_roi_set = QPushButton("Set Acquisition ROI")
+        self.btn_roi_set.clicked.connect(self.setROIAcquisition)
+        # Form layout
+        flo_roi = QFormLayout()
+        flo_roi.addRow(lbl_roi_x, self.edt_roi_x)
+        flo_roi.addRow(lbl_roi_y, self.edt_roi_y)
+        flo_roi.addRow(lbl_roi_width, self.edt_roi_width)
+        flo_roi.addRow(lbl_roi_height, self.edt_roi_height)
+        # Vertical box
+        vbox_roi = QVBoxLayout()
+        vbox_roi.addLayout(flo_roi)
+        vbox_roi.addWidget(self.btn_roi_set)
+        # GroupBox
+        group_roi = QGroupBox("ROI")
+        group_roi.setLayout(vbox_roi)
+        return group_roi
+
+    def setDMDHome(self):
         # DMD section is added using the form option
         # Labels
-        #lbl_dmd = QLabel("DMD")
+        # lbl_dmd = QLabel("DMD")
         lbl_dmd_irr = QLabel("Irradiation")
         lbl_dmd_iper = QLabel("Irradiation Period")
         lbl_dmd_pulse = QLabel("Pulse Duration")
         lbl_dmd_led = QLabel("LED Intensity")
         # Edit lines
-        edt_dmd_irr = QLineEdit()
-        edt_dmd_irr.setValidator(QIntValidator())
-        edt_dmd_irr.setAlignment(Qt.AlignRight)
-        edt_dmd_irr.setMaxLength(3)
-        edt_dmd_iper = QLineEdit()
-        edt_dmd_iper.setValidator(QIntValidator())
-        edt_dmd_iper.setAlignment(Qt.AlignRight)
-        edt_dmd_iper.setMaxLength(3)
-        edt_dmd_pulse = QLineEdit()
-        edt_dmd_pulse.setValidator(QIntValidator())
-        edt_dmd_pulse.setAlignment(Qt.AlignRight)
-        edt_dmd_pulse.setMaxLength(3)
-        edt_dmd_led = QLineEdit()
-        edt_dmd_led.setValidator(QIntValidator())
-        edt_dmd_led.setAlignment(Qt.AlignRight)
-        edt_dmd_led.setMaxLength(3)
+        self.edt_dmd_irr = QLineEdit()
+        self.edt_dmd_irr.setValidator(QIntValidator())
+        self.edt_dmd_irr.setAlignment(Qt.AlignRight)
+        self.edt_dmd_irr.setMaxLength(3)
+        self.edt_dmd_irr.setText('0')
+        self.edt_dmd_iper = QLineEdit()
+        self.edt_dmd_iper.setValidator(QIntValidator())
+        self.edt_dmd_iper.setAlignment(Qt.AlignRight)
+        self.edt_dmd_iper.setMaxLength(3)
+        self.edt_dmd_iper.setText('0')
+        self.edt_dmd_pulse = QLineEdit()
+        self.edt_dmd_pulse.setValidator(QIntValidator())
+        self.edt_dmd_pulse.setAlignment(Qt.AlignRight)
+        self.edt_dmd_pulse.setMaxLength(3)
+        self.edt_dmd_pulse.setText('0')
+        self.edt_dmd_led = QLineEdit()
+        self.edt_dmd_led.setValidator(QIntValidator())
+        self.edt_dmd_led.setAlignment(Qt.AlignRight)
+        self.edt_dmd_led.setMaxLength(3)
+        self.edt_dmd_led.setText('0')
         # Buttons
-        btn_dmd_roi = QPushButton("Set Activation ROI")
+        self.btn_dmd_roi = QPushButton("Set Activation ROI")
         # Form layout
         flo_dmd = QFormLayout()
-        flo_dmd.addRow(lbl_dmd_irr, edt_dmd_irr)
-        flo_dmd.addRow(lbl_dmd_iper, edt_dmd_iper)
-        flo_dmd.addRow(lbl_dmd_pulse, edt_dmd_pulse)
-        flo_dmd.addRow(lbl_dmd_led, edt_dmd_led)
+        flo_dmd.addRow(lbl_dmd_irr, self.edt_dmd_irr)
+        flo_dmd.addRow(lbl_dmd_iper, self.edt_dmd_iper)
+        flo_dmd.addRow(lbl_dmd_pulse, self.edt_dmd_pulse)
+        flo_dmd.addRow(lbl_dmd_led, self.edt_dmd_led)
         # Vertical box
         vbox_dmd = QVBoxLayout()
-        vbox_dmd.addWidget(btn_dmd_roi)
+        vbox_dmd.addWidget(self.btn_dmd_roi)
         vbox_dmd.addLayout(flo_dmd)
         # GroupBox
         group_dmd = QGroupBox("DMD")
         group_dmd.setLayout(vbox_dmd)
+        return group_dmd
 
-        # Adding elements to the left main box
-        vbox_main.addStretch()
-        vbox_main.addWidget(group_cam)
-        vbox_main.addStretch()
-        vbox_main.addWidget(group_dmd)
-        vbox_main.addStretch()
-
-        # Right main box
+    def setExpHome(self):
         # Optogenetic Experiment section
         # Labels
-        #lbl_exp = QLabel("Optogenetic Experiment")
+        # lbl_exp = QLabel("Optogenetic Experiment")
         lbl_exp_pre_seq = QLabel("Preactivation Sequence")
         lbl_exp_pre_num = QLabel("Nb. Im.")
         lbl_exp_pre_exp = QLabel("Exposure")
@@ -114,16 +192,16 @@ class window(QWidget):
         lbl_exp_pos_num = QLabel("Nb. Im.")
         lbl_exp_pos_exp = QLabel("Exposure")
         # Checkboxes
-        chck_exp_pre_seq = QCheckBox()
-        chck_exp_pre_num = QCheckBox()
-        chck_exp_pre_exp = QCheckBox()
-        chck_exp_act = QCheckBox()
-        chck_exp_pos_seq = QCheckBox()
-        chck_exp_pos_num = QCheckBox()
-        chck_exp_pos_exp = QCheckBox()
+        self.chck_exp_pre_seq = QCheckBox()
+        self.chck_exp_pre_num = QCheckBox()
+        self.chck_exp_pre_exp = QCheckBox()
+        self.chck_exp_act = QCheckBox()
+        self.chck_exp_pos_seq = QCheckBox()
+        self.chck_exp_pos_num = QCheckBox()
+        self.chck_exp_pos_exp = QCheckBox()
         # Buttons
-        btn_exp_start = QPushButton("Launch")
-        btn_exp_stop = QPushButton("Stop")
+        self.btn_exp_start = QPushButton("Launch")
+        self.btn_exp_stop = QPushButton("Stop")
         # Hboxes EXP
         hbox_exp_pre = QHBoxLayout()
         hbox_exp_pos = QHBoxLayout()
@@ -132,69 +210,86 @@ class window(QWidget):
         vbox_exp = QVBoxLayout()
         # Setting horizontal boxes
         hbox_exp_pre.addWidget(lbl_exp_pre_seq)
-        hbox_exp_pre.addWidget(chck_exp_pre_seq)
+        hbox_exp_pre.addWidget(self.chck_exp_pre_seq)
         hbox_exp_pre.addWidget(lbl_exp_pre_num)
-        hbox_exp_pre.addWidget(chck_exp_pre_num)
+        hbox_exp_pre.addWidget(self.chck_exp_pre_num)
         hbox_exp_pre.addWidget(lbl_exp_pre_exp)
-        hbox_exp_pre.addWidget(chck_exp_pre_exp)
+        hbox_exp_pre.addWidget(self.chck_exp_pre_exp)
 
         hbox_exp_act.addWidget(lbl_exp_act)
-        hbox_exp_act.addWidget(chck_exp_act)
+        hbox_exp_act.addWidget(self.chck_exp_act)
         hbox_exp_act.addStretch()
 
         hbox_exp_pos.addWidget(lbl_exp_pos_seq)
-        hbox_exp_pos.addWidget(chck_exp_pos_seq)
+        hbox_exp_pos.addWidget(self.chck_exp_pos_seq)
         hbox_exp_pos.addWidget(lbl_exp_pos_num)
-        hbox_exp_pos.addWidget(chck_exp_pos_num)
+        hbox_exp_pos.addWidget(self.chck_exp_pos_num)
         hbox_exp_pos.addWidget(lbl_exp_pos_exp)
-        hbox_exp_pos.addWidget(chck_exp_pos_exp)
+        hbox_exp_pos.addWidget(self.chck_exp_pos_exp)
         # Setting vertical box
         vbox_exp.addStretch()
-        #vbox_exp.addWidget(lbl_exp)
-        vbox_exp.addWidget(btn_exp_start)
-        vbox_exp.addWidget(btn_exp_stop)
+        # vbox_exp.addWidget(lbl_exp)
+        vbox_exp.addWidget(self.btn_exp_start)
+        vbox_exp.addWidget(self.btn_exp_stop)
         vbox_exp.addLayout(hbox_exp_pre)
         vbox_exp.addLayout(hbox_exp_act)
         vbox_exp.addLayout(hbox_exp_pos)
         # Groupbox
         group_exp = QGroupBox("Optogenetic Experiment")
         group_exp.setLayout(vbox_exp)
+        return group_exp
 
+    def setGraphHome(self):
         # Graph section to be located on top of the EXP section
         fig = Figure()
-        ax = fig.add_subplot(111)
-        ax.clear()
-        ax.plot(x, y, 'bo-')
-        graph = FigureCanvas(fig)
+        self.ax = fig.add_subplot(111)
+        self.graph = FigureCanvas(fig)
+        self.displayImage()
         # Navigation widget
         # it takes the Canvas widget and a parent
-        toolbar = NavigationToolbar(graph, self)
+        toolbar = NavigationToolbar(self.graph, self)
         # Graph Vbox
         vbox_graph = QVBoxLayout()
         vbox_graph.addWidget(toolbar)
-        vbox_graph.addWidget(graph)
+        vbox_graph.addWidget(self.graph)
         # Groupbox
         group_graph = QGroupBox("Image Display")
         group_graph.setLayout(vbox_graph)
-        #Main right VBox
-        vbox_main_exp = QVBoxLayout()
-        vbox_main_exp.addWidget(group_graph)
-        vbox_main_exp.addStretch()
-        vbox_main_exp.addWidget(group_exp)
+        return group_graph
 
-        #Main HBOX containing all subelements
-        hbox_main = QHBoxLayout()
-        hbox_main.addLayout(vbox_main)
-        hbox_main.addStretch()
-        hbox_main.addLayout(vbox_main_exp)
-        self.setLayout(hbox_main)
+    def snapImage(self):
+        self.cameraModel.snapImage()
+        self.img = self.cameraModel.getImage()
 
-        self.show()
+    def displayImage(self):
+        self.ax.clear()
+        self.ax.imshow(self.img, cmap='gray')
+        self.graph.draw_idle()
+
+    # Interface Controller methods start here
+    # Retrieves values from GUI and start the sequence acquisition
+    def startSequenceAcquisition(self):
+        exposureTime = int(self.edt_cam_exp.text())
+        numImages = int(self.edt_cam_num.text())
+        self.cameraModel.setExposureTime(exposureTime)
+        self.cameraModel.setNumImages(numImages)
+        self.cameraModel.startSequenceAcquisition()
+        self.cameraModel.resetCore()
+    # Retrieves the values from GUI and sets a new ROI
+    def setROIAcquisition(self):
+        print('pressed')
+        x = int(self.edt_roi_x.text())
+        y = int(self.edt_roi_y.text())
+        xSize = int(self.edt_roi_width.text())
+        ySize = int(self.edt_roi_height.text())
+        self.cameraModel.setROI(x, y, xSize, ySize)
+        self.snapImage()
+        self.displayImage()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    x = np.linspace(1, 5, num=60, endpoint=True)
-    y = np.sin(2 * np.pi * x)
-    GUI = window()
+    #x = np.linspace(1, 5, num=60, endpoint=True)
+    #y = np.sin(2*np.pi*x)
+    GUI = Window()
     sys.exit(app.exec_())
---
+++ acquisitionModel.py
@@ -0,0 +1,127 @@
+# This python code uses MicroManager device drivers to control a camera.
+# Tested on Hamamatsu Orca Flash 4 camera, it acquires a sequence of images and
+# stores them in a folder. ROI will also be tested.
+# Code implementation: David Sanchez
+
+# code used to get an image sequence acquisition
+
+# Importing necessary libraries
+import sys
+import MMCorePy  # load MicroManager for device control
+import matplotlib.pyplot as plt
+from pylab import *
+import time
+from PIL import Image
+
+
+class AcquisitionModel:
+
+    def __init__(self):
+        sys.path.append("C:\\Program Files\\Micro-Manager-1.4")
+        # loading camera
+        self.mmc = MMCorePy.CMMCore()
+        self.mmc.getVersionInfo()
+        self.mmc.loadDevice('Camera', 'DemoCamera', 'DCam')
+        # Hamamatsu
+        # self.mmc.loadDevice('Camera', 'HamamatsuHam', 'HamamatsuHam_DCAM'
+        self.mmc.initializeAllDevices()
+        self.mmc.setCameraDevice('Camera')
+        self.x = 0
+        self.y = 0
+        self.xSize = int(self.mmc.getProperty('Camera', 'OnCameraCCDXSize'))
+        self.ySize = int(self.mmc.getProperty('Camera', 'OnCameraCCDYSize'))
+        self.expTime = 1
+        self.numImages = 1
+        self.intervalMs = 1
+
+    def setROI(self, x, y, xSize, ySize):
+        self.x = x
+        self.y = y
+        self.xSize = xSize
+        self.ySize = ySize
+        self.mmc.setROI(x, y, xSize, ySize)
+
+    def setExposureTime(self, expTime):
+        self.expTime = expTime
+        self.mmc.setExposure(self.expTime)
+
+    def setNumImages(self, numImages):
+        self.numImages = numImages
+
+    def setIntervalMs(self, intervalMs):
+        self.intervalMs = intervalMs
+
+    def getXSize(self):
+        return self.xSize
+
+    def getYSize(self):
+        return self.ySize
+
+    def snapImage(self):
+        self.mmc.snapImage()
+        self.img = self.mmc.getImage()  # this is numpy array, by the way
+
+    def getImage(self):
+        return self.img
+
+    def startSequenceAcquisition(self):
+        # Image sequence acquisition
+        start_time = time.time()
+        self.mmc.clearCircularBuffer()
+        self.mmc.startSequenceAcquisition(self.numImages, self.intervalMs, 1) # 1 is stopOnOverflow parameter
+
+        # print mmc.getRemainingImageCount()
+
+        self.waitAcquisition()
+        print("--- %s seconds ---" % (time.time() - start_time))
+        imList = []
+        #plt.close('all')
+        for x in range(self.numImages):
+            # print mmc.getRemainingImageCount()
+            img = self.mmc.popNextImage()
+            # filename = "C:\\Users\\MOTIV\\Documents\\Python\\image%d.tiff" % (x,)
+            filename = "sequence.tiff"
+            # filename = "C:\\Users\\Administrateur\\Documents\\David\\image%d.tiff" % (x,)
+            imList.append(Image.fromarray(img))
+            imList[0].save(filename, compression="tiff_deflate", save_all=True,
+                           append_images=imList[1:])
+        #print self.mmc.getRemainingImageCount()
+
+    def resetCore(self):
+        self.mmc.reset()
+
+    def waitAcquisition(self):
+        while True:
+            if self.mmc.isSequenceRunning():
+                time.sleep(0.0001)
+            else:
+                break
+
+    def imgNormalization(self, arr):
+        arr = arr.astype('float')
+        minVal = arr.min()
+        maxVal = arr.max()
+        maxLim = 2 ** 16
+        if minVal != maxVal:
+            arr -= minVal
+            arr *= (maxLim / (maxVal - minVal))
+        return arr.astype('uint16')
+
+
+
+if __name__ == "__main__":
+    cameraModel = AcquisitionModel()
+    print(cameraModel.mmc.getImageHeight())
+    #cameraModel.setROI(0, 0, 200, 100)
+    #print(cameraModel.mmc.getImageHeight())
+    #print(cameraModel.mmc.getProperty('Camera', 'OnCameraCCDXSize'))
+    #cameraModel.snapImage()
+    numImages = 10
+    intervalMs = 1
+    exposureTime = 10
+    cameraModel.setNumImages(numImages)
+    cameraModel.setExposureTime(exposureTime)
+    cameraModel.startSequenceAcquisition()
+    cameraModel.resetCore()
+
+
