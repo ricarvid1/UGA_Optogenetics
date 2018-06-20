073406cfa098b1d97c28329a21a59d34c34a5cab
DAVID_UGA <david-ricardo.sanchez-jacome@etu.univ-amu.fr>
2018-06-20 16:07:42 +0200

    AcquisitionModel was renamed to avoid conflicts

--- AcquisitionModel.py
+++ AcquisitionModel.py
@@ -20,18 +20,18 @@ class AcquisitionModel:
         # loading camera
         self.mmc = MMCorePy.CMMCore()
         self.mmc.getVersionInfo()
-        #self.mmc.loadDevice('Camera', 'DemoCamera', 'DCam')
+        # self.mmc.loadDevice('Camera', 'DemoCamera', 'DCam')
         # Hamamatsu
         self.mmc.loadDevice('Camera', 'HamamatsuHam', 'HamamatsuHam_DCAM')
         self.mmc.initializeAllDevices()
         self.mmc.setCameraDevice('Camera')
-        self.mmc.setCircularBufferMemoryFootprint(2048) #Buffer siwe is set to 1Gb acquisitions with mqny frames
+        self.mmc.setCircularBufferMemoryFootprint(2048)  # Buffer siwe is set to 1Gb acquisitions with mqny frames
         self.x = 0
         self.y = 0
         self.xSize = int(self.mmc.getImageWidth())
         self.ySize = int(self.mmc.getImageHeight())
-        self.width = self.xSize # ROI width
-        self.height = self.ySize # ROI height
+        self.width = self.xSize  # ROI width
+        self.height = self.ySize  # ROI height
         self.expTime = 1
         self.numImages = 1
         self.intervalMs = 1
@@ -39,23 +39,22 @@ class AcquisitionModel:
         self.filename = "sequence.tiff"
         self.numAcquisitions = 0
 
-
     def setROI(self, x, y, width, height):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.mmc.setROI(x, y, width, height)
-        
+
     def setX(self, x):
         self.x = x
-        
+
     def setY(self, y):
         self.y = y
-        
+
     def setWidth(self, width):
         self.width = width
-        
+
     def setHeight(self, height):
         self.height = height
 
@@ -87,22 +86,22 @@ class AcquisitionModel:
 
     def startSequenceAcquisition(self):
         # Image sequence acquisition
-        #Camerq is reloaded in case there are 2 or more consecutive acquisitions
+        # Camerq is reloaded in case there are 2 or more consecutive acquisitions
         if self.numAcquisitions > 0:
             self.reloadCamera()
             self.setROI(self.x, self.y, self.width, self.height)
-            
+
         self.successfulAcquisition = False
         print("Acquiring %d Images. Exposure time: %d ms" % (self.numImages, self.expTime))
         self.mmc.waitForDevice('Camera')
         self.mmc.prepareSequenceAcquisition('Camera')
-        #self.mmc.initializeDevice('Camera')
-        #self.mmc.setCameraDevice('Camera')
+        # self.mmc.initializeDevice('Camera')
+        # self.mmc.setCameraDevice('Camera')
         start_time = time.time()
         self.mmc.prepareSequenceAcquisition('Camera')
-        self.mmc.startSequenceAcquisition(self.numImages, self.intervalMs, False) # 1 is stopOnOverflow parameter
+        self.mmc.startSequenceAcquisition(self.numImages, self.intervalMs, False)  # 1 is stopOnOverflow parameter
 
-        #self.waitAcquisition()
+        # self.waitAcquisition()
         imList = []
         # filename = "C:\\Users\\MOTIV\\Documents\\Python\\image%d.tiff" % (x,)
         # filename = "C:\\Users\\Administrateur\\Documents\\David\\image%d.tiff" % (x,)
@@ -119,17 +118,17 @@ class AcquisitionModel:
                                    append_images=imList[1:])
                     print("File saved after %s seconds" % (time.time() - start_time))
                     break
-                
+
         print("Remaining Images: %d" % self.mmc.getRemainingImageCount())
-        self.mmc.waitForDevice('Camera') # The program waits until the device is done with all its tasks
+        self.mmc.waitForDevice('Camera')  # The program waits until the device is done with all its tasks
         self.numAcquisitions += 1
-        
+
     def reloadCamera(self):
         self.mmc.unloadDevice('Camera')
-        self.mmc.loadDevice('Camera', 'HamamatsuHam', 'HamamatsuHam_DCAM')    
+        self.mmc.loadDevice('Camera', 'HamamatsuHam', 'HamamatsuHam_DCAM')
         self.mmc.initializeDevice('Camera')
         self.mmc.setCameraDevice('Camera')
-            
+
     def resetCore(self):
         self.mmc.reset()
 
@@ -151,12 +150,11 @@ class AcquisitionModel:
         return arr.astype('uint16')
 
 
-
 if __name__ == "__main__":
     cameraModel = AcquisitionModel()
     numImages = 2
     exposureTime = 10
-    #print(cameraModel.getXSize())
+    # print(cameraModel.getXSize())
     cameraModel.setROI(0, 0, 100, 100)
     cameraModel.setNumImages(numImages)
     cameraModel.setExposureTime(exposureTime)
@@ -164,6 +162,4 @@ if __name__ == "__main__":
     cameraModel.filename = "sequence2.tiff"
     cameraModel.startSequenceAcquisition()
     cameraModel.resetCore()
-    #cameraModel.startSequenceAcquisition()
-
-
+    # cameraModel.startSequenceAcquisition()
