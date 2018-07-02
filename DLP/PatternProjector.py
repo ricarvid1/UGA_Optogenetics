ee8322a5c428a084e0c2188b61b152ac91b9f514
DAVID_UGA <david-ricardo.sanchez-jacome@etu.univ-amu.fr>
2018-07-02 14:02:59 +0200

    Pattern control implemented with pilot window. It needs to be tested in the lab.

--- DLP/PatternProjector.py
+++ DLP/PatternProjector.py
@@ -3,97 +3,116 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 import numpy as np
 import matplotlib.pyplot as plt
 
-import random
 
 
-class Window(QWidget):
+class PatternWindow(QWidget):
     def __init__(self, parent=None):
-        super(Window, self).__init__(parent)
-
+        super(PatternWindow, self).__init__(parent)
+        # this new window has no frame
         self.setWindowFlags(Qt.FramelessWindowHint)
-
         # a figure instance to plot on
         self.figure = Figure()
-
         # this is the Canvas Widget that displays the `figure`
         # it takes the `figure` instance as a parameter to __init__
         self.canvas = FigureCanvas(self.figure)
+        # create an axis with equal aspect ratio and also on the given position
+        self.ax = self.figure.add_axes([0., 0., 1., 1.], aspect='equal')
+        # discards the old graph
+        self.ax.clear()
+        # axis are removed to show the pattern
+        self.ax.axis('off')
+        # both face color and background (patch) are set to black
+        self.ax.set_facecolor('k')
+        self.figure.patch.set_color('k')
 
-        # this is the Navigation widget
-        # it takes the Canvas widget and a parent
-
-        # Just some button connected to `plot` method
-        self.plot()
         # set the layout
         layout = QVBoxLayout()
         layout.addWidget(self.canvas)
         hlayout = QHBoxLayout()
-        #hlayout.addStretch()
         hlayout.addLayout(layout)
-        #hlayout.addStretch()
         self.setLayout(hlayout)
 
+    # Generates the desired pattern and plots it on the secondary window
     def plot(self):
-        ''' plot some random stuff '''
-        #data = [random.random() for i in range(10)]
+        # A pattern in plotted
+        # A circle is plotted
+        xSize = 1024
+        xCenter = xSize / 2
+        ySize = 1024
+        yCenter = ySize / 2
+        radius = 256
+        self.data = np.zeros((xSize, ySize))
+        for i in range(xSize):
+            for j in range(ySize):
+                d = np.sqrt((i - xCenter)**2 + (j - yCenter)**2)
+                if d < radius:
+                    self.data[i, j] = 1
+
+        # data (matrix) is displayed like an image
+        self.ax.imshow(self.data, cmap='gray')
+        # refresh canvas
+        self.canvas.draw_idle()
+
+    # Resets the pattern on the secondary window to a black screen
+    def reset(self):
         xSize = 1024
         xCenter = xSize / 2
         ySize = 1024
         yCenter = ySize / 2
         radius = 256
         data = np.zeros((xSize, ySize))
-        for i in range(xSize):
-            for j in range(ySize):
-                d = np.sqrt((i - xCenter)**2 + (j - yCenter)**2)
-                if d < radius:
-                    data[i, j] = 1
-
-        # create an axis
-        #ax = self.figure.add_subplot(111)
-        ax = self.figure.add_axes([0., 0., 1., 1.], aspect='equal')
-
-        # discards the old graph
-        ax.clear()
 
         # plot data
-        ax.imshow(data, cmap='gray')
-        #ax.plot(data)
-        ax.axis('off')
-        ax.set_facecolor('k')
-        self.figure.patch.set_color('k')
-        print(ax.get_facecolor())
-        '''
-        fig = plt.figure()
-        ax = plt.axes([0, 0, 1, 1])
-        plt.imshow(data, interpolation="nearest")
-        plt.axis("off")
-        '''
-        # refresh canvas
+        self.ax.imshow(data, cmap='gray')
         self.canvas.draw_idle()
 
+# Controller window used for testing purposes
+class smallWindow(QMainWindow):
+    def __init__(self, parent=None):
+        # init and geometry settings
+        super(smallWindow, self).__init__(parent)
+        self.setGeometry(100, 100, 100, 100)
+        self.setWindowTitle('Control test window')
+        # secondary window used to show the pattern
+        self.pattern = PatternWindow()
+        # window is placed on the secondary desktop screen to be projected
+        pDesktop = QApplication.desktop()
+        RectScreen1 = pDesktop.screenGeometry(1)
+        self.pattern.move(RectScreen1.left(), RectScreen1.top())
+        self.pattern.resize(RectScreen1.width(), RectScreen1.height())
+        # background of window is set to black
+        palette = self.pattern.palette()
+        palette.setColor(self.main.backgroundRole(), Qt.black)
+        self.pattern.setPalette(palette)
+        # pattern is shown
+        self.pattern.showMaximized()
+        # controlling test buttons
+        self.button = QPushButton("Set Pattern ")
+        self.button.clicked.connect(self.plot)
+
+        self.btn_reset = QPushButton("Reset")
+        self.btn_reset.clicked.connect(self.reset)
+        # central widget is created
+        self.center = QWidget()
+        self.setCentralWidget(self.center)
+        # set layout
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.button)
+        vbox.addWidget(self.btn_reset)
+        self.center.setLayout(vbox)
+
+    def plot(self):
+        self.main.plot()
+
+    def reset(self):
+        self.main.reset()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
-    main = Window()
-    pDesktop = QApplication.desktop()
-    RectScreen1 = pDesktop.screenGeometry(1)
-    main.move(RectScreen1.left(), RectScreen1.top())
-    main.resize(RectScreen1.width(), RectScreen1.height())
-    palette = main.palette()
-    palette.setColor(main.backgroundRole(), Qt.black)
-    main.setPalette(palette)
-
-    main.showMaximized()
-
-    win2 = Window()
-    win2.setGeometry(100, 100, 100, 100)
-    win2.setWindowTitle('Control test window')
-
-
+    win2 = smallWindow()
+    win2.show()
     sys.exit(app.exec_())
